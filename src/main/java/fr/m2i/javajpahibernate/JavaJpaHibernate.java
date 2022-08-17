8f46c09b7e5b8ddb414982650b16da2be35344b0
Tiity <cadet_a@etna-alternance.net>
2022-08-17 10:12:39 +0200

    JPA setup

--- src/main/java/fr/m2i/javajpahibernate/JavaJpaHibernate.java
+++ src/main/java/fr/m2i/javajpahibernate/JavaJpaHibernate.java
@@ -1,8 +1,14 @@
 package fr.m2i.javajpahibernate;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class JavaJpaHibernate {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("site_commerce_pu");
+        EntityManager entityManager = emf.createEntityManager();
+        System.out.println("Hello world!");
     }
 }
